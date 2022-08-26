443cd0e90c2b7e2335170b34887b1a7407096682
Sai Pavan Puligadda <56425243+saipavan1160@users.noreply.github.com>
2022-08-26 12:19:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V3! tada!')
+  res.send('CICD App V4! tada!')
 })
 
 app.get('/status', (req, res) => {
